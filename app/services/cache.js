ae1d3264cedcedbc897b1f1424bb8c629004caad
AymerickCotche <aymerick.cotche@hotmail.fr>
2022-01-21 16:16:14 +0100

    added redis error catch

--- app/services/cache.js
+++ app/services/cache.js
@@ -4,6 +4,10 @@ if (process.env.NODE_ENV === 'production') {
     config.url = process.env.REDIS_URL
 }
 const db = createClient(config);
+db.on('error', err => {
+    console.log('Redis err')
+    console.log(err)
+})
 db.connect();
 
 module.exports = db;
