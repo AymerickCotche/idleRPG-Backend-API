455cbfc05a655adb33882d9b0e9c8bd3d5c3891d
AymerickCotche <aymerick.cotche@hotmail.fr>
2022-01-31 16:30:31 +0100

    rollback to take of items at the initial loading

--- app/controllers/itemController.js
+++ app/controllers/itemController.js
@@ -4,11 +4,13 @@ const dbCache = require('../services/cache');
 
 module.exports = {
 
-    async findAll(request, response, next) {
+    async findAll(request, response) {
         try {
             const items = await Item.findAll();
-            response.locals.items = items;
-            next();
+            const token = jwt.makeToken(request.userId);
+            response.setHeader('Authorization', jwt.makeToken(request.userId));
+            await dbCache.set("user-0"+request.userId, token, {EX: 4*60*60, NX: false});
+            response.json(items);
         } catch (error) {
             console.log(error);
             response.status(500).json(error.message);
