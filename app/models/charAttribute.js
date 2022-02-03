36681c85a3c27d81312f55e7a81364486605b9f8
AymerickCotche <aymerick.cotche@hotmail.fr>
2022-02-03 14:15:57 +0100

    adding quantity to attribute update

--- app/models/charAttribute.js
+++ app/models/charAttribute.js
@@ -10,7 +10,7 @@ class CharAttribute {
 
     async updateIncrement() {
         try {
-            await db.query('SELECT * FROM updateAttribute($1, $2)', [Number(this.attributeId), Number(this.characterId)]);
+            await db.query('SELECT * FROM updateAttribute($1, $2, $3)', [Number(this.attributeId), Number(this.characterId), Number(this.quantity)]);
         } catch (error) {
             console.log(error);
             if (error.detail) {
