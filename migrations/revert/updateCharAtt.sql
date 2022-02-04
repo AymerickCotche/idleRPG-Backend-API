26ec85f0a92ff73e9e9c5e5824281daaa57fe025
AymerickCotche <aymerick.cotche@hotmail.fr>
2022-02-04 12:26:45 +0100

    add comment to revert on heroku

--- migrations/revert/updateCharAtt.sql
+++ migrations/revert/updateCharAtt.sql
@@ -3,6 +3,6 @@
 BEGIN;
 
 DROP FUNCTION updateAttribute(id_attribute INT, id_character INT, quantity INT);
-DROP FUNCTION updateHp(id_character INT, new_hp INT);
+-- DROP FUNCTION updateHp(id_character INT, new_hp INT);
 
 COMMIT;
