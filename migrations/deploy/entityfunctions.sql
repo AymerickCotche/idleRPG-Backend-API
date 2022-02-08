b921ba8410a8db0a62c37d0d7415e09a6dae4dc8
AymerickCotche <aymerick.cotche@hotmail.fr>
2022-02-08 16:19:35 +0100

    modifying function to get entity with items

--- migrations/deploy/entityfunctions.sql
+++ migrations/deploy/entityfunctions.sql
@@ -21,9 +21,17 @@ CREATE OR REPLACE FUNCTION getAllEntities()
 			JOIN attribute ON attribute.id = entity_attribute.attribute_id
 			),
 			rewards_items AS
-			(SELECT entity_reward_item.id, entity_reward_item.quantity, entity_reward_item.entity_id, entity_reward_item.drop_rate, item.name AS item_name, item.id AS item_id, item_type.id AS item_type_id, item_type.name AS item_type_name  FROM entity_reward_item
+			(WITH item_att AS
+				(SELECT item_attribute.id, item_attribute.value, item_attribute.item_id, attribute.name FROM item_attribute
+					JOIN attribute ON attribute.id = item_attribute.attribute_id 
+				)
+			SELECT entity_reward_item.id, entity_reward_item.quantity, entity_reward_item.entity_id, entity_reward_item.drop_rate, item.name AS item_name, item.id AS item_id, item_type.id AS item_type_id, item_type.name AS item_type_name, item.desc AS item_desc,
+			jsonb_agg(DISTINCT to_jsonb(item_att) - 'item_id') AS attributes
+			FROM entity_reward_item
 			JOIN item ON item.id = entity_reward_item.item_id
 			JOIN item_type ON item_type.id = item.item_type_id
+			JOIN item_att  ON item_att.item_id = item.id
+			 GROUP BY entity_reward_item.id, item.name, item.id, item_type.id
 			)
 		SELECT entity.*,
 			jsonb_agg(DISTINCT to_jsonb(entity_att) - 'entity_id') AS attributes,
