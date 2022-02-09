bb5e095634f08937d0f9a02783f44df92b2c0e1e
AymerickCotche <aymerick.cotche@hotmail.fr>
2022-02-09 11:18:39 +0100

    changing model function to update HP when using a cons

--- app/models/charAttribute.js
+++ app/models/charAttribute.js
@@ -1,60 +1,71 @@
 const db = require('../database');
 
 class CharAttribute {
+	constructor(obj = {}) {
+		for (const prop in obj) {
+			this[prop] = obj[prop];
+		}
+	}
 
-    constructor(obj={}) {
-        for (const prop in obj) {
-            this[prop] = obj[prop];
-        }
-    }
+	async updateIncrement() {
+		try {
+			await db.query('SELECT * FROM updateAttribute($1, $2, $3)', [
+				Number(this.attributeId),
+				Number(this.characterId),
+				Number(this.quantity),
+			]);
+		} catch (error) {
+			console.log(error);
+			if (error.detail) {
+				throw new Error(error.detail);
+			}
+			throw error;
+		}
+	}
 
-    async updateIncrement() {
-        try {
-            await db.query('SELECT * FROM updateAttribute($1, $2, $3)', [Number(this.attributeId), Number(this.characterId), Number(this.quantity)]);
-        } catch (error) {
-            console.log(error);
-            if (error.detail) {
-                throw new Error(error.detail)
-            }
-            throw error
-        }
-    }
+	async updateHp() {
+		try {
+			await db.query('SELECT * FROM updateHp($1, $2)', [
+				Number(this.characterId),
+				Number(this.newHp),
+			]);
+		} catch (error) {
+			console.log(error);
+			if (error.detail) {
+				throw new Error(error.detail);
+			}
+			throw error;
+		}
+	}
 
-    async updateHp() {
-        try {
-            await db.query('SELECT * FROM updateHp($1, $2)', [Number(this.characterId), Number(this.newHp)]);
-        } catch (error) {
-            console.log(error);
-            if (error.detail) {
-                throw new Error(error.detail)
-            }
-            throw error
-        }
-    }
+	async augmentHp() {
+		try {
+			await db.query('SELECT * FROM updateHp(11, $1, $2)', [
+				Number(this.characterId),
+				Number(this.plusHp),
+			]);
+		} catch (error) {
+			console.log(error);
+			if (error.detail) {
+				throw new Error(error.detail);
+			}
+			throw error;
+		}
+	}
 
-    async augmentHp() {
-        try {
-            await db.query('SELECT * FROM updateAttribute(11, $1, $2)', [Number(this.characterId), Number(this.plusHp)]);
-        } catch (error) {
-            console.log(error);
-            if (error.detail) {
-                throw new Error(error.detail)
-            }
-            throw error
-        }
-    }
-
-    async updateStatsPoints() {
-        try {
-            await db.query('SELECT * FROM updateStatsPoints($1)', [Number(this.characterId)]);
-        } catch (error) {
-            console.log(error);
-            if (error.detail) {
-                throw new Error(error.detail)
-            }
-            throw error
-        }
-    }
+	async updateStatsPoints() {
+		try {
+			await db.query('SELECT * FROM updateStatsPoints($1)', [
+				Number(this.characterId),
+			]);
+		} catch (error) {
+			console.log(error);
+			if (error.detail) {
+				throw new Error(error.detail);
+			}
+			throw error;
+		}
+	}
 }
 
 module.exports = CharAttribute;
