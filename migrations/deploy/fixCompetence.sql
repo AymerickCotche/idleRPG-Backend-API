d126ea128a5f7ee5d9fb018b21ee2c3450e8df16
AymerickCotche <aymerick.cotche@hotmail.fr>
2022-03-03 07:35:36 +0100

    Fixing increment_cost for competence table

--- migrations/deploy/fixCompetence.sql
+++ migrations/deploy/fixCompetence.sql
@@ -6,4 +6,8 @@ ALTER TABLE "competence"
   ALTER COLUMN increment_effect
   TYPE DECIMAL;
 
+ALTER TABLE "competence"
+  ALTER COLUMN increment_cost
+  TYPE DECIMAL;
+
 COMMIT;
--- migrations/revert/fixCompetence.sql
+++ migrations/revert/fixCompetence.sql
@@ -6,4 +6,8 @@ ALTER TABLE "competence"
   ALTER COLUMN increment_effect
   TYPE INT;
 
+ALTER TABLE "competence"
+  ALTER COLUMN increment_cost
+  TYPE INT;
+
 COMMIT;
